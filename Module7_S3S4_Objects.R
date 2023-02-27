9dfccfe6036383965f6e489fa598863957498229
nullwijn <56175027+nullwijn@users.noreply.github.com>
2023-02-26 23:56:39 -0500

    Add comments

--- Module7_S3S4_Objects.R
+++ Module7_S3S4_Objects.R
@@ -25,23 +25,29 @@ tree1S4
 
 #UseMethod() and SetGeneric()
 #UseMethod()
+
+#Set up the pointer function
 my_S3function <- function(x) {
   UseMethod("my_S3function")
 }
 
+#Use the tree class I created earlier, create a concatenated summary of the data.
 my_S3function.treeS3 <- function(x) {
   paste("This is an", x$type, "tree that is", x$age, "months old and has a circumference of", x$circumference)
 }
-
+#Run the function with my S3 object
 my_S3function(tree1S3)
 
 #SetGeneric()
+
+#Set up the S4 version of the pointer function
 setGeneric("my_S4function", function(x) {
   standardGeneric("my_S4function")
 })
 
+#Set up the s4 version of the concatenation, using the same S4 class I defined earlier as well
 setMethod("my_S4function", "treeS4", function(x){
   paste("This is an", x@type, "tree that is", x@age, "months old and has a circumference of", x@circumference)
 })
-
+#Run the function with my S4 object
 my_S4function(tree1S4)
