a4564e5242dca4ae58019bc2de75df3b295551a7
nullwijn <56175027+nullwijn@users.noreply.github.com>
2023-02-26 23:54:48 -0500

    Add step 5

--- Module7_S3S4_Objects.R
+++ Module7_S3S4_Objects.R
@@ -5,20 +5,43 @@ list(Orange, 6)
 #Yes, generic functions work
 
 #S3 class assignment
-tree1 <- list(type="Orange",age=Orange$age[1],circumference=Orange$circumference[1])
-class(tree1) <- "tree"
-tree1
-attributes(tree1)
+tree1S3 <- list(type="Orange",age=Orange$age[1],circumference=Orange$circumference[1])
+class(tree1S3) <- "treeS3"
+tree1S3
+attributes(tree1S3)
 #S3 works on Orange
 
 #S4 class assignment
-setClass("tree",
+setClass("treeS4",
          representation(
            type="character",
            age="numeric",
            circumference="numeric"
          ))
-tree1 <- new("tree",type="Orange",age=Orange$age[1],circumference=Orange$circumference[1])
-isS4(tree1)
-tree1
+tree1S4 <- new("treeS4",type="Orange",age=Orange$age[1],circumference=Orange$circumference[1])
+isS4(tree1S4)
+tree1S4
 #S4 works on Orange
+
+#UseMethod() and SetGeneric()
+#UseMethod()
+my_S3function <- function(x) {
+  UseMethod("my_S3function")
+}
+
+my_S3function.treeS3 <- function(x) {
+  paste("This is an", x$type, "tree that is", x$age, "months old and has a circumference of", x$circumference)
+}
+
+my_S3function(tree1S3)
+
+#SetGeneric()
+setGeneric("my_S4function", function(x) {
+  standardGeneric("my_S4function")
+})
+
+setMethod("my_S4function", "treeS4", function(x){
+  paste("This is an", x@type, "tree that is", x@age, "months old and has a circumference of", x@circumference)
+})
+
+my_S4function(tree1S4)
