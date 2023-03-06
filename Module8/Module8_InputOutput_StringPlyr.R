ffbb8f1bc7138102e3caad6e8612672225babf76
nullwijn <56175027+nullwijn@users.noreply.github.com>
2023-03-06 02:39:28 -0500

    Add module 8

---
+++ Module8/Module8_InputOutput_StringPlyr.R
@@ -0,0 +1,17 @@
+library(plyr)
+library(data.table)
+
+myTable <- read.table("Assignment6Dataset.txt", header = TRUE, sep = ",")
+#I kept getting errors when trying to use the code provided on the assignment page, so I decided to list the comma as a separator to fix the data.
+#There are a lot of spelling errors and capitilization problems in the assignment page, so I can't tell if the functions are also typed
+#incorrectly or not.
+
+StudentAverage = ddply(myTable,"Sex",summarize,Grade.Average=mean(Grade))
+#Using summarize instead of transform makes it a simple 1x2 table instead of a clone of the first table with the average
+#tacked on to each student.
+
+write.table(StudentAverage, "Students_Gendered_Mean")
+
+i_students <- subset(as.data.frame(myTable), grepl("i", Name, ignore.case=T))
+
+write.csv(i_students, "I_Students")
